ed5187e231d4f2d24521244cf37bf4d6c9dbd515
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2024-04-06 03:32:24 +0530

    Changed window

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,16 +7,18 @@ import { useState } from "react";
 const Nav = () => {
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
-  window.addEventListener('scroll', () => {
-    var nav = document.getElementById('nav')
-    if(window.scrollY > 0){
-      var classList = ['flex-between w-full py-3 px-6 fixed top-0 z-10 border-b border-gray-200 transition-all bg-white/0 backdrop-blur shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]']
-      nav.classList = classList
-    }else{
-      var classList = ['flex-between w-full py-3 px-6 fixed top-0 z-10 transition-all']
-      nav.classList = classList
-    }
-  })
+  if(typeof window !== 'undefined'){
+    window.addEventListener('scroll', () => {
+      var nav = document.getElementById('nav')
+      if(window.scrollY > 0){
+        var classList = ['flex-between w-full py-3 px-6 fixed top-0 z-10 border-b border-gray-200 transition-all bg-white/0 backdrop-blur shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]']
+        nav.classList = classList
+      }else{
+        var classList = ['flex-between w-full py-3 px-6 fixed top-0 z-10 transition-all']
+        nav.classList = classList
+      }
+    })
+  }
 
   return (
     <nav className="flex-between w-full py-3 px-6 fixed top-0 z-10 transition-all h-[5rem]" id="nav" >
